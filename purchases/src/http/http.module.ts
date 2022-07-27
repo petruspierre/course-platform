40526cac4e8d84bef5f01f7e1cf26bc2b24eac49
Petrus Pierre <petrus.tsx@gmail.com>
2022-07-27 17:56:11 -0300

    Initial commit

---
+++ purchases/src/http/http.module.ts
@@ -0,0 +1,34 @@
+import { ApolloDriver } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import path from 'node:path';
+
+import { DatabaseModule } from 'src/database/database.module';
+import { CustomersService } from 'src/services/customers.service';
+import { ProductsService } from 'src/services/products.service';
+import { PurchasesService } from 'src/services/purchases.service';
+import { CustomersResolver } from './graphql/resolvers/customers.resolver';
+import { ProductsResolver } from './graphql/resolvers/products.resolver';
+import { PurchaseResolver } from './graphql/resolvers/purchases.resolver';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  providers: [
+    ProductsResolver,
+    PurchaseResolver,
+    CustomersResolver,
+
+    ProductsService,
+    PurchasesService,
+    CustomersService,
+  ],
+})
+export class HttpModule {}
---
+++ purchases/src/services/purchases.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+
+import { PrismaService } from 'src/database/prisma/prisma.service';
+
+interface CreatePurchaseParams {
+  customerId: string;
+  productId: string;
+}
+
+@Injectable()
+export class PurchasesService {
+  constructor(private prisma: PrismaService) {}
+
+  async listAllPurchases() {
+    return this.prisma.purchase.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+  }
+
+  async listAllFromCustomer(customerId: string) {
+    return this.prisma.purchase.findMany({
+      where: {
+        customerId,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+  }
+
+  async createPurchase({ customerId, productId }: CreatePurchaseParams) {
+    const product = this.prisma.product.findUnique({
+      where: {
+        id: productId,
+      },
+    });
+
+    if (!product) {
+      throw new Error('Product not found.');
+    }
+
+    return await this.prisma.purchase.create({
+      data: {
+        productId,
+        customerId,
+      },
+    });
+  }
+}
